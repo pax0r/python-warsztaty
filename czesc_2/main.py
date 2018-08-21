c520ca8aa7083ecd6ef4d61231b8af64866be196
Bartlomiej Biernacki <Bartlomiej Biernacki>
2018-08-21 20:54:34 +0200

    Waz wersja ostateczna

--- czesc_2/main.py
+++ czesc_2/main.py
@@ -15,13 +15,23 @@ class MainWindow(tkinter.Frame):
         self.canvas = tkinter.Canvas(self, width=self.CANVAS_WIDTH,
                                      height=self.CANVAS_HEIGHT)
         self.canvas.pack()
+
+        self.przycisk_losuj = tkinter.Button(self, text="Losuj", command=self.losuj)
+        self.przycisk_losuj.pack()
+        self.przycisk_idz = tkinter.Button(self, text="Idz", command=self.zrob_krok)
+        self.przycisk_idz.pack()
+        self.przycisk_czysc = tkinter.Button(self, text="Czysc", command=self.czysc_plansze)
+        self.przycisk_czysc.pack()
+
+        self.waz = None
+        self.jedzonko = None
+        self.losuj()
+
+    def losuj(self):
         self.waz = waz.Waz.losuj_punkt(self.CANVAS_WIDTH, self.CANVAS_HEIGHT)
         self.jedzonko = waz.Punkt.losuj_punkt(self.CANVAS_WIDTH, self.CANVAS_HEIGHT)
-
         self.narysuj_weza()
         self.narysuj_jedzonko()
-
-        self.zrob_krok()
 
     def narysuj_weza(self):
         self.canvas.create_rectangle(self.waz.x, self.waz.y, self.waz.x, self.waz.y,
@@ -34,9 +44,15 @@ class MainWindow(tkinter.Frame):
 
     def zrob_krok(self):
         if self.waz.x != self.jedzonko.x or self.waz.y != self.jedzonko.y:
+            self.przycisk_czysc["state"] = tkinter.DISABLED
             self.waz.zrob_krok(self.jedzonko)
             self.narysuj_weza()
             self.after(20, self.zrob_krok)
+        else:
+            self.przycisk_czysc["state"] = tkinter.ACTIVE
+
+    def czysc_plansze(self):
+        self.canvas.delete('all')
 
 
 if __name__ == '__main__':

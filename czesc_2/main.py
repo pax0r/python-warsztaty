2c2e96e62cbf38ee04402ec8121cdbd70268c2ba
Bartlomiej Biernacki <Bartlomiej Biernacki>
2018-08-21 20:34:11 +0200

    Waz WIP

--- czesc_2/main.py
+++ czesc_2/main.py
@@ -15,6 +15,20 @@ class MainWindow(tkinter.Frame):
         self.canvas = tkinter.Canvas(self, width=self.CANVAS_WIDTH,
                                      height=self.CANVAS_HEIGHT)
         self.canvas.pack()
+        self.waz = waz.Waz.losuj_punkt(self.CANVAS_WIDTH, self.CANVAS_HEIGHT)
+        self.jedzonko = waz.Punkt.losuj_punkt(self.CANVAS_WIDTH, self.CANVAS_HEIGHT)
+
+        self.narysuj_weza()
+        self.narysuj_jedzonko()
+
+    def narysuj_weza(self):
+        self.canvas.create_rectangle(self.waz.x, self.waz.y, self.waz.x, self.waz.y,
+                                     outline='blue')
+
+    def narysuj_jedzonko(self):
+        self.canvas.create_rectangle(self.jedzonko.x, self.jedzonko.y,
+                                     self.jedzonko.x, self.jedzonko.y,
+                                     outline='red')
 
 
 if __name__ == '__main__':
